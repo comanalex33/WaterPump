69736d45a3363fa7a8e545d3a40957e5ba547eaa
Coman Alex <comanalex33@yahoo.com>
2022-05-23 01:13:33 +0300

    Added date of chart element on click

--- Software/app/src/main/java/app/quic/waterpump/SecondActivity.kt
+++ Software/app/src/main/java/app/quic/waterpump/SecondActivity.kt
@@ -3,27 +3,29 @@ package app.quic.waterpump
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import app.quic.waterpump.models.ThingSpeakResponse
 import app.quic.waterpump.services.ApiClient
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.github.mikephil.charting.utils.ColorTemplate
-import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+
 
 class SecondActivity : AppCompatActivity() {
 
@@ -34,6 +36,7 @@ class SecondActivity : AppCompatActivity() {
     val handler = Handler(Looper.getMainLooper())
     private lateinit var backButton: Button
     private lateinit var averageText: TextView
+    private lateinit var elementDateText: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +45,7 @@ class SecondActivity : AppCompatActivity() {
 
         backButton = findViewById(R.id.back_button)
         averageText = findViewById(R.id.textView2)
+        elementDateText = findViewById(R.id.element_date)
 
         handler.post(object : Runnable {
             override fun run() {
@@ -54,6 +58,7 @@ class SecondActivity : AppCompatActivity() {
                         response: Response<ThingSpeakResponse>
                     ) {
                         if (response.isSuccessful) {
+
                             linelist= ArrayList()
                             for(i in 0..99){
                                 linelist.add(Entry(i.toFloat(),response.body()?.feeds!![i].field2.toFloat()))
@@ -67,6 +72,20 @@ class SecondActivity : AppCompatActivity() {
                             lineDataset.setColors(*ColorTemplate.JOYFUL_COLORS)
                             lineDataset.valueTextColor= Color.BLUE
                             lineDataset.valueTextSize= 20f
+
+                            val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.UK)
+                            format.timeZone = TimeZone.getTimeZone("UTC")
+                            val outputFormat = SimpleDateFormat("yyyy MMMM dd - HH:mm")
+
+                            line_chart.setOnChartValueSelectedListener(object :
+                                OnChartValueSelectedListener {
+                                override fun onValueSelected(e: Entry, h: Highlight?) {
+                                    val date = format.parse(response.body()?.feeds!![e.x.toInt()].created_at)
+                                    elementDateText.text = outputFormat.format(date!!)
+                                }
+
+                                override fun onNothingSelected() {}
+                            })
 
 
                         } else {
